30a711dd3ecc6ff8ef8e396d7f86ebcd43bbe3be
zaim <abuzaim12@gmail.com>
2014-12-26 23:08:21 +0800

    Use param-case package

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,13 @@ var gulp     = require('gulp');
 var rename   = require('gulp-rename');
 var template = require('gulp-template');
 var merge    = require('merge-stream');
+var dashed   = require('param-case');
 var palettes = require('./index');
 
-function camelToDash (str) {
-  return str
-    .replace(/[_\s]+/g, '-')        // '_' and spaces to single dash
-    .replace(/([A-Z])/g, '-$1')     // 'A' -> '-A'
-    .replace(/(\d)(\d*)/, '-$1$2')  // '500' -> '-500'
-    .replace(/-+/g, '-')            // '---' -> '-'
-    .replace(/([A-Z])-/, '$1')      // 'orange-A-500' -> 'orange-A500'
-    .toLowerCase();
-}
-
-function camelToDashKeys (obj) {
+function dashedKeys (obj) {
   var newObj = {};
   Object.keys(obj).forEach(function (key) {
-    newObj[camelToDash(key)] = obj[key];
+    newObj[dashed(key)] = obj[key];
   });
   return newObj;
 }
@@ -27,7 +18,7 @@ gulp.task('default', function () {
     return gulp
       .src('./templates/*.tpl')
       .pipe(template({
-        colors: camelToDashKeys(palettes[title])
+        colors: dashedKeys(palettes[title])
       }))
       .pipe(rename(function (path) {
         path.dirname  = title;
