5bf595cc2c92abe5ac5cd4b4147c38129a041d43
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-02-24 22:18:46 +0800

    chore: fix test/color.js using eslint

--- test/colors.js
+++ test/colors.js
@@ -1,36 +1,34 @@
 /* global describe, it */
 
-var assert   = require('assert');
-var palettes = require('..');
+var assert = require("assert");
+var palettes = require("..");
 
 var DEFAULT_COLORS = [
-  'aqua',
-  'blue',
-  'lime',
-  'navy',
-  'teal',
-  'olive',
-  'green',
-  'red',
-  'maroon',
-  'orange',
-  'purple',
-  'yellow',
-  'fuchsia',
-  'gray',
-  'white',
-  'black',
-  'silver'
+  "aqua",
+  "blue",
+  "lime",
+  "navy",
+  "teal",
+  "olive",
+  "green",
+  "red",
+  "maroon",
+  "orange",
+  "purple",
+  "yellow",
+  "fuchsia",
+  "gray",
+  "white",
+  "black",
+  "silver"
 ];
 
 var HEX = /#([0-9A-F]{2}){3}/;
 
-
-describe('palettes', function () {
-
-  it('should have all default colors', function () {
-    Object.keys(palettes).forEach(function (title) {
-      DEFAULT_COLORS.forEach(function (name) {
+describe("palettes", function() {
+  it("should have all default colors", function() {
+    Object.keys(palettes).forEach(function(title) {
+      DEFAULT_COLORS.forEach(function(name) {
         var value = palettes[title][name];
         assert.ok(
           value && value.length,
@@ -40,16 +38,21 @@ describe('palettes', function () {
     });
   });
 
-  it('should have full hex values', function () {
-    Object.keys(palettes).forEach(function (title) {
-      Object.keys(palettes[title]).forEach(function (name) {
+  it("should have full hex values", function() {
+    Object.keys(palettes).forEach(function(title) {
+      Object.keys(palettes[title]).forEach(function(name) {
         assert.ok(
           HEX.test(palettes[title][name]),
-          'Palette color ' + title + '.' + name + ' is not a hex value ' +
-          '(' + palettes[title][name] + ')'
+          "Palette color " +
+            title +
+            "." +
+            name +
+            " is not a hex value " +
+            "(" +
+            palettes[title][name] +
+            ")"
         );
       });
     });
   });
-
 });
